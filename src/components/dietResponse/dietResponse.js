76f244c165338527d48fcb9eaaea788f02961872
Paweł Uchański <uchanski.pawel@gmail.com>
2023-04-27 00:22:02 +0200

    Adjustments

--- src/components/dietResponse/dietResponse.js
+++ src/components/dietResponse/dietResponse.js
@@ -1,19 +1,27 @@
 import classes from './dietResponse.module.css'
 import { saveDataToStore } from '../../utils/utils';
+import { useState } from 'react';
 
 const DietResponse = (props) => {
+  const [saved, setSaved] = useState(false);
 
   const saveHandler = () => {
+    setSaved(true);
     saveDataToStore('diets','diet', props.dietPlan);
   }
 
+  const buttonClasses = `${classes.button} ${saved ? classes.disabled : ''}`
 
   return (
     <div className={classes.wrapper}>
       <h1>Your plan:</h1>
-      <p>{props.dietPlan ? props.dietPlan : 'No data' }</p>
+      <div className={classes.response}>
+        {props.dietPlan ? props.dietPlan : 'No data' }
+      </div>
 
-      <button onClick={saveHandler} className={classes.button}>Save diet</button>
+
+      <button onClick={saveHandler} className={buttonClasses}>Save diet</button>
+      {saved && <p className={classes.success}>Diet saved sucessfully</p>}
     </div>
   )
 }
--- src/components/recipeResponse/recipeResponse.js
+++ src/components/recipeResponse/recipeResponse.js
@@ -1,17 +1,24 @@
 import classes from './recipeResponse.module.css'
 import { saveDataToStore } from '../../utils/utils';
+import { useState } from 'react';
 
 const RecipeResponse = (props) => {
+  const [saved, setSaved] = useState(false);
+
   const saveHandler = () => {
+    setSaved(true);
     saveDataToStore('recipes','recipe', props.recipe);
   }
+
+  const buttonClasses = `${classes.button} ${saved ? classes.disabled : ''}`
 
   return (
     <div className={classes.wrapper}>
       <h1>Your recipe</h1>
       <p>{props.recipe ? props.recipe : 'No data' }</p>
 
-      <button onClick={saveHandler} className={classes.button}>Save recipe</button>
+      <button onClick={saveHandler} className={buttonClasses} disabled={saved}>Save recipe</button>
+      {saved && <p className={classes.success}>Recipe saved succesfully</p>}
     </div>
   )
 
